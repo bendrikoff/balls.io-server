13ab14d5e3d608eede4e9f118ac07712589d85ff
Aleksandr.Bendrikov <bendrikoff0220@gmail.com>
2025-12-01 14:19:15 +0300

    cors

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,7 @@
 import config from "@colyseus/tools";
 import { monitor } from "@colyseus/monitor";
 import { playground } from "@colyseus/playground";
+import cors from "cors";
 
 /**
  * Import your Room files
@@ -14,34 +15,30 @@ export default config({
          * Define your room handlers:
          */
         gameServer.define('my_room', MyRoom);
-
     },
 
     initializeExpress: (app) => {
-        /**
-         * Bind your custom express routes here:
-         * Read more: https://expressjs.com/en/starter/basic-routing.html
-         */
+
+        // ⭐️⭐️⭐️ ВАЖНО: Добавляем CORS для всех запросов ⭐️⭐️⭐️
+        app.use(cors({
+            origin: "*",          // можно указать домен: "https://remix.gg"
+            methods: ["GET", "POST", "OPTIONS"],
+            credentials: false
+        }));
+
+        // или super-простой вариант:
+        // app.use(cors());
+
         app.get("/hello_world", (req, res) => {
             res.send("It's time to kick ass and chew bubblegum!");
         });
 
-        /**
-         * Use @colyseus/playground
-         * (It is not recommended to expose this route in a production environment)
-         */
         if (process.env.NODE_ENV !== "production") {
             app.use("/", playground());
         }
 
-        /**
-         * Use @colyseus/monitor
-         * It is recommended to protect this route with a password
-         * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
-         */
         app.use("/monitor", monitor());
     },
-
 
     beforeListen: () => {
         /**
